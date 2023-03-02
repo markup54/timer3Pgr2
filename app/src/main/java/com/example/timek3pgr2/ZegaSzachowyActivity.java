3cafb1e00db979135c53391ad8bdb464fda38679
markup54 <marzenakupka@tlen.pl>
2023-03-02 16:12:42 +0100

    zega szachowy prawie gotowy

--- app/src/main/java/com/example/timek3pgr2/ZegaSzachowyActivity.java
+++ app/src/main/java/com/example/timek3pgr2/ZegaSzachowyActivity.java
@@ -3,12 +3,42 @@ package com.example.timek3pgr2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ZegaSzachowyActivity extends AppCompatActivity {
-
+Button button;
+TextView textView1;
+TextView textView2;
+Zegar zegar1;
+Zegar zegar2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zega_szachowy);
+        textView1 = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        zegar1 = new Zegar(true,textView1);
+        zegar1.startTimer();
+        zegar2 = new Zegar(false,textView2);
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(zegar1.isRunning()){
+                            zegar1.stopTimer();
+                            zegar2.startTimer();
+                            button.setText("GRACZ 2");
+                        }
+                        else{
+                            zegar2.stopTimer();
+                            zegar1.startTimer();
+                            button.setText("GRACZ 1");
+                        }
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/example/timek3pgr2/Zegar.java
+++ app/src/main/java/com/example/timek3pgr2/Zegar.java
@@ -1,2 +1,50 @@
-package com.example.timek3pgr2;public class Zegar {
+package com.example.timek3pgr2;
+
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+public class Zegar {
+    private boolean running;
+    private int sekundy = 180;
+    private TextView timeTextView;
+    private CountDownTimer timer;
+
+    public Zegar(boolean running, TextView timeTextView) {
+        this.running = running;
+        this.timeTextView = timeTextView;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+    private void pokazIleZostalo(int s){
+        int minuty = s/60;
+        int sek = s%60;
+        timeTextView.setText(String.format("%02d : %02d",minuty,sek));
+    }
+    public  void startTimer(){
+        timer = new CountDownTimer(sekundy*1000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                sekundy =(int) millisUntilFinished/1000;
+                pokazIleZostalo(sekundy);
+            }
+
+            @Override
+            public void onFinish() {
+                timeTextView.setText("KONIEC");
+                //TODO wyłączyć guzik itp
+            }
+        };
+        timer.start();
+        running = true;
+    }
+    public void stopTimer(){
+        timer.cancel();
+        running = false;
+    }
 }
