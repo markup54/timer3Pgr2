2f5b1e5284a12e30e970143990afcf48e930b965
markup54 <marzenakupka@tlen.pl>
2023-03-02 16:12:24 +0100

    zega szachowy prawie gotowy

---
+++ app/src/main/java/com/example/timek3pgr2/ZegaSzachowyActivity.java
@@ -0,0 +1,14 @@
+package com.example.timek3pgr2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ZegaSzachowyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zega_szachowy);
+    }
+}
---
+++ app/src/main/java/com/example/timek3pgr2/Zegar.java
@@ -0,0 +1,2 @@
+package com.example.timek3pgr2;public class Zegar {
+}
